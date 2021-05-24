de3ae7061a4ac2dd06dabc58c5d89e578e8e5c46
protect-me <pmfwiww@gmail.com>
2021-05-24 20:47:44 +0900

    Feat: View Engine(Nunjucks)

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
+
 const admin = require("./routes/admin.js");
 
 const app = express();
 const port = 3000;
+
+nunjucks.configure("template", {
+  autoescape: true,
+  express: app,
+});
+// 폴더명 : template
+// 보안유지 : autoescape
+// express : app 할당
 
 app.get("/", (req, res) => {
   res.send("hello express!");
--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,10 @@ router.get("/", (req, res) => {
 });
 
 router.get("/products", (req, res) => {
-  res.send("admin / products page!");
+  // res.send("admin / products page!");
+  res.render("admin/products.html", {
+    message: "hello:)",
+  });
 });
 
 module.exports = router;
